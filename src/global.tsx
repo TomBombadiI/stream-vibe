f6a561ee4ce8142ce816e6b844e22595e021f59f
TomBombadiI <78550692+TomBombadiI@users.noreply.github.com>
2025-03-07 18:25:21 +0700

    update: add global layout

---
+++ src/global.tsx
@@ -0,0 +1,36 @@
+import { Head } from "minista";
+import Header from "@/layouts/Header";
+import Footer from "@/layouts/Footer";
+import Content from "@/layouts/Content";
+import '@/styles';
+
+export default function (props: any) {
+    const {
+        title,
+        children
+    } = props
+
+    return (
+        <>
+            <Head htmlAttributes={{ lang: 'en' }}>
+                <title>StramVibe :: {title}</title>
+
+                <link rel="preconnect" href="https://fonts.googleapis.com" />
+                <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
+                <link href="https://fonts.googleapis.com/css2?family=Manrope:wght@200..800&display=swap" rel="stylesheet" />
+
+                <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+                <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+                <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+                <link rel="manifest" href="/site.webmanifest" />
+
+                <script src="/src/main.js" type="module" />
+            </Head>
+            <Header />
+            <Content>
+                {children}
+            </Content>
+            <Footer />
+        </>
+    )
+}
