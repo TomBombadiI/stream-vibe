d7327f295c4cc951e5af1d9e4ef90db1601eb08b
TomBombadiI <78550692+TomBombadiI@users.noreply.github.com>
2025-03-07 20:08:57 +0700

    2.1 Sticky эффект, применение scroll animation-timeline

--- src/layouts/Header/Header.tsx
+++ src/layouts/Header/Header.tsx
@@ -1,3 +1,13 @@
-export const Header = () => {
-    return <header>HEADER</header>
+import './Header.scss';
+
+const Header = () => {
+    return (
+        <header className='header'>
+            <div className="header__inner container">
+                HEADER
+            </div>
+        </header>
+    )
 }
+
+export default Header;
