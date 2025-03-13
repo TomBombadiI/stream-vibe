012da716d23edef9403ec946eab9f704e811ebe2
TomBombadiI <78550692+TomBombadiI@users.noreply.github.com>
2025-03-13 22:08:13 +0700

    3.3 Секция Categories — компоненты CategoryCard и Image

--- src/sections/Categories/Categories.tsx
+++ src/sections/Categories/Categories.tsx
@@ -1,7 +1,56 @@
 import Section from '@/layouts/Section';
 import './Categories.scss';
+import CategoryCard from '@/components/CategoryCard';
 
 const Categories = () => {
+    const categoryItems = [
+        {
+            title: 'Action',
+            images: [
+                '/src/assets/images/categories/action/1.jpg',
+                '/src/assets/images/categories/action/2.jpg',
+                '/src/assets/images/categories/action/3.jpg',
+                '/src/assets/images/categories/action/4.jpg',
+            ]
+        },
+        {
+            title: 'Adventure',
+            images: [
+                '/src/assets/images/categories/action/1.jpg',
+                '/src/assets/images/categories/action/2.jpg',
+                '/src/assets/images/categories/action/3.jpg',
+                '/src/assets/images/categories/action/4.jpg',
+            ]
+        },
+        {
+            title: 'Comedy',
+            images: [
+                '/src/assets/images/categories/action/1.jpg',
+                '/src/assets/images/categories/action/2.jpg',
+                '/src/assets/images/categories/action/3.jpg',
+                '/src/assets/images/categories/action/4.jpg',
+            ]
+        },
+        {
+            title: 'Drama',
+            images: [
+                '/src/assets/images/categories/action/1.jpg',
+                '/src/assets/images/categories/action/2.jpg',
+                '/src/assets/images/categories/action/3.jpg',
+                '/src/assets/images/categories/action/4.jpg',
+            ]
+        },
+        {
+            title: 'Horror',
+            images: [
+                '/src/assets/images/categories/action/1.jpg',
+                '/src/assets/images/categories/action/2.jpg',
+                '/src/assets/images/categories/action/3.jpg',
+                '/src/assets/images/categories/action/4.jpg',
+            ]
+        },
+    ];
+
     return (
         <Section
             title="Explore our wide variety of categories"
@@ -11,11 +60,16 @@ const Categories = () => {
                 <div>
                     <button>Назад</button>
                     <button>Вперед</button>
-                </div>  
+                </div>
             )}
             isActionsVisibleOnMobile={false}
         >
-            categories
+            {categoryItems.map((categoryItem, index) => (
+                <CategoryCard 
+                    {...categoryItem}
+                    key={index}
+                />
+            ))}
         </Section>
     )
 }
