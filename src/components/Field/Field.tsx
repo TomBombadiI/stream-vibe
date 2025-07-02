f9d95d60b9c7ba8f00bef53e3c598bbd096c90cd
TomBombadiI <78550692+TomBombadiI@users.noreply.github.com>
2025-07-02 10:21:57 +0700

    7.1 Секция Support — компонент Field (2 шага)

---
+++ src/components/Field/Field.tsx
@@ -0,0 +1,56 @@
+import getIdFromTitle from '@/utils/getIdFromTitle';
+import './Field.scss';
+import classNames from 'classnames';
+
+type FieldProps = {
+  id?: string,
+  className: string,
+  label: string,
+  type?: 'text' | 'email' | 'textarea',
+  placeholder?: string,
+  isRequired?: boolean,
+  inputMode?: "text" | "email" | "search" | "tel" | "none" | "url" | "numeric" | "decimal"
+}
+
+const Field = (props: FieldProps) => {
+  const {
+    id = getIdFromTitle(props.label),
+    className,
+    label,
+    type = 'text',
+    placeholder,
+    isRequired = false,
+    inputMode
+  } = props;
+
+  const Component = type === 'textarea' ? 'textarea' : 'input';
+
+  return (
+    <div
+      className={classNames(className, 'field')}
+    >
+      <label
+        className='field__label'
+        htmlFor={id} >
+        {label} {isRequired && (
+          <span
+            className='field__required-star'
+            aria-hidden={true}
+          >*</span>
+        )}
+      </label>
+      <div className="field__body">
+        <Component
+          className='field__control'
+          id={id}
+          type={type}
+          placeholder={placeholder}
+          required={isRequired}
+          inputMode={inputMode}
+        />
+      </div>
+    </div>
+  )
+}
+
+export default Field;
---
+++ src/pages/support.jsx
@@ -0,0 +1,13 @@
+import Support from '@/sections/Support';
+
+export const metadata = {
+  title: 'Support Page'
+}
+
+export default function () {
+  return (
+    <>
+      <Support />
+    </>
+  )
+}
